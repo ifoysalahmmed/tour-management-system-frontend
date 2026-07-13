4ff0f1749d2b1875c87b13a46a97bc1e2f5511c5
Foysal Ahmmed Limon <afaysal410@gmail.com>
2026-07-13 15:46:01 +0600

    feat: implement authentication modules with login and registration pages and reusable form field components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router";
 
 import App from "@/App";
 import About from "@/pages/About";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
 
 export const router = createBrowserRouter([
   {
@@ -13,5 +15,13 @@ export const router = createBrowserRouter([
         Component: About,
       },
     ],
+  },
+  {
+    path: "/login",
+    Component: Login,
+  },
+  {
+    path: "/register",
+    Component: Register,
   },
 ]);
