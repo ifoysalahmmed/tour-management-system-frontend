7011f569b92a3870dbbabcf1d82bb77a6d666716
Foysal Ahmmed Limon <afaysal410@gmail.com>
2026-07-18 15:07:53 +0600

    feat: enhance auth API with sendOTP, verifyOTP, and resendOTP mutations

--- src/redux/features/auth/auth.api.ts
+++ src/redux/features/auth/auth.api.ts
@@ -1,22 +1,58 @@
 import { baseApi } from "@/redux/baseApi";
+import type {
+  ILogin,
+  ILoginResponse,
+  IRegister,
+  IRegisterResponse,
+  ISendOTP,
+  IVerifyOTP,
+  Root,
+} from "@/types";
 
 const authApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    login: builder.mutation({
+    login: builder.mutation<Root<ILoginResponse>, ILogin>({
       query: (credentials) => ({
         url: "/auth/login",
         method: "POST",
         data: credentials,
       }),
     }),
-    register: builder.mutation({
+    register: builder.mutation<Root<IRegisterResponse>, IRegister>({
       query: (userInfo) => ({
         url: "/user/register",
         method: "POST",
         data: userInfo,
       }),
     }),
+    sendOTP: builder.mutation<Root<null>, ISendOTP>({
+      query: (userEmail) => ({
+        url: "/otp/send",
+        method: "POST",
+        data: userEmail,
+      }),
+    }),
+    verifyOTP: builder.mutation<Root<null>, IVerifyOTP>({
+      query: (otpData) => ({
+        url: "/otp/verify",
+        method: "POST",
+        data: otpData,
+      }),
+    }),
+    resendOTP: builder.mutation<Root<null>, ISendOTP>({
+      query: (userEmail) => ({
+        url: "/otp/send",
+        method: "POST",
+        data: userEmail,
+      }),
+    }),
   }),
 });
 
-export const { useLoginMutation, useRegisterMutation } = authApi;
+export const {
+  useLoginMutation,
+  useRegisterMutation,
+  useSendOTPMutation,
+  useVerifyOTPMutation,
+  useResendOTPMutation,
+} = authApi;
