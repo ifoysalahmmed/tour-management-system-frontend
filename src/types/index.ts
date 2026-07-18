65f560a47cc8523e30b2cd76cc2f514cc6dca30d
Foysal Ahmmed Limon <afaysal410@gmail.com>
2026-07-18 15:07:31 +0600

    feat: add authentication types for login, registration, and OTP verification

---
+++ src/types/index.ts
@@ -0,0 +1,30 @@
+export type {
+  ILogin,
+  ILoginResponse,
+  IRegister,
+  IRegisterResponse,
+  ISendOTP,
+  IVerifyOTP,
+} from "./auth.type";
+
+export interface TMeta {
+  total: number;
+  page?: number;
+  limit?: number;
+  totalPages?: number;
+}
+
+export interface TErrorSource {
+  path: string | number;
+  message: string;
+}
+
+export interface Root<T = unknown> {
+  statusCode: number;
+  success: boolean;
+  message: string;
+  data?: T;
+  meta?: TMeta;
+  errorSources?: TErrorSource[];
+  stack?: string;
+}
